b6f4d3246018ef394399a9f32e93bc1e5f6f6905
TomSieg <tom.sieg@wanadoo.fr>
2015-04-02 15:32:31 +0200

    1.2.3

--- ressource/connection.js
+++ ressource/connection.js
@@ -1,16 +1,18 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection(
 {
-host : 'localhost',
-user : 'root',
-password : '',
-database : 'stocks',
+	host : 'localhost',
+	user : 'root',
+	password : '',
+	database : 'stocks',
 }
 );
+
 var selectuser = function (login, password, callback){
-var queryString = 'SELECT id, login, password, access from user WHERE login = "'+ login + '"';
-connection.query(queryString, function(err, rows, fields) {
-callback(rows[0]);
-});
+	var queryString = 'SELECT id, login, password, access from user WHERE login = "'+ login + '"';
+	connection.query(queryString, function(err, rows, fields) {
+		callback(rows[0]);
+	});
 };
+
 exports.selectuser = selectuser;
