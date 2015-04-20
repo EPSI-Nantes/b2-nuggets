9ea012741cbdc7b104b91ef46b6bb2d896c647da
Antoine Garrec <antoine.garrec@epsi.fr>
2015-04-20 16:11:02 +0200

    V1.4
    page avec les equipement

--- ressource/connection.js
+++ ressource/connection.js
@@ -4,17 +4,28 @@ var connection = mysql.createConnection(
       host     : 'localhost',
       user     : 'root',
       password : '',
-      database : 'stocks',
+      database : 'nuggets',
     }
 );
 
-var selectuser = function (login, password, callback){
-  var queryString = 'SELECT id, login, password, access from user WHERE login = "'+ login + '"';
-	connection.query(queryString, function(err, rows, fields) {
-    callback(rows[0]);
+
+
+var login = function (login, password, callback){
+connection.query('SELECT id, access from user WHERE login = ? AND password = ?',[ login , password],  function(err, rows, fields) {
+try{
+  if (rows[0].access == "admin" || rows[0].access == "user") {
+    var msg = rows[0].access;
+    callback(msg);
+};
+}
+catch(err){
+    var msg = 'Le login ou mot de pass incorecte';
+    callback(msg);
+  }
 });
 };
-exports.selectuser = selectuser;
+exports.login = login;
+
 
 var afficheruser = function (callback){
   var queryString = 'SELECT id, login from user';
@@ -24,9 +35,28 @@ var afficheruser = function (callback){
 };
 exports.afficheruser = afficheruser;
 
+var lshardware = function (callback){
+  var queryString = 'SELECT id, name, ip from equipement';
+  connection.query(queryString, function(err, rows, fields) {
+    callback(rows);
+});
+};
+exports.lshardware = lshardware;
+
+var deletuser = function (useradelet){ 
+  connection.query('DELETE FROM user WHERE id = ? ;', [ useradelet ],  function(err, rows, fields) {
+});
+};
+exports.deletuser = deletuser;
+
+var delethardware = function (hardwareadelet){
+  connection.query('DELETE FROM equipement WHERE id = ? ;', [ hardwareadelet ],  function(err, rows, fields) {
+});
+};
+exports.delethardware = delethardware;
+
 var ajoutuser = function (loginajouter, passwordajouter, access, callback){
-  var queryString = 'INSERT INTO user (login, password, access) VALUES ("'+ loginajouter +'", "'+ passwordajouter +'", "'+ access +'");';
-	connection.query(queryString, function(err, fields) {
+connection.query('INSERT INTO user (login, password, access) VALUES (?, ?, ?);',[ loginajouter , passwordajouter, access],  function(err, rows, fields) {
     if (err) {
     var msg = 'il y a une erreur';
     callback(msg);
@@ -39,17 +69,8 @@ var ajoutuser = function (loginajouter, passwordajouter, access, callback){
 };
 exports.ajoutuser = ajoutuser;
 
-var deletuser = function (useradelet, callback){
-  var queryString = 'DELETE FROM user WHERE id='+ useradelet + '';
-  connection.query(queryString, function(err, fields) {
-  if (err) {
-    var msg = 'il y a une erreur';
-    callback(msg);
-  }
-  else {
-    var msg = 'Done';
-    callback(msg);
-  }
+var ajouterhardware = function (nameeq, ip){
+connection.query('INSERT INTO equipement (name, ip) VALUES (?, ?);',[ nameeq , ip],  function(err, rows, fields) {
 });
 };
-exports.deletuser = deletuser;
+exports.ajouterhardware = ajouterhardware;
--- serveur.js
+++ serveur.js
@@ -9,16 +9,60 @@ var server = http.createServer(app);
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var sess;
 var io = require('socket.io').listen(server);
-
+var lsavions;
+var lsproduit = '';
+var lsaeroports;
 app.use(session({secret: 'todotopsecret'}));
 app.use(bodyParser.urlencoded({
-  extended: true
+extended: true
 }));
 app.use(bodyParser.json());
+
+
 
 app.get('/', function(req, res) {
 var msg ='';
 res.render('index.ejs', {msg: msg});
+});
+
+app.post('/delethardware', function(req, res) {
+  var hardwareadelet = req.body.hardwareadelet;
+  reqmysql.delethardware(hardwareadelet);
+res.redirect('/hardware');
+});
+
+app.get('/hardware', function(req, res) {
+reqmysql.lshardware(function callback (result){
+var hardware = result;
+res.render('hardware.ejs', {msg: hardware});
+});
+});
+
+app.post('/ajouterhardware', function(req, res) {
+  var nameeq = req.body.nameeq;
+  var ip = req.body.ip;
+  reqmysql.ajouterhardware(nameeq, ip);
+res.redirect('/hardware');
+});
+
+app.post('/connexion', function(req, res) {
+  var login = req.body.login;
+  var password = req.body.password;
+  reqmysql.login(login, password, function callback (result){
+if (result == "admin" || result == "user") {
+    sess = req.session;
+    sess.access = result.access;
+    res.render('admin.ejs', {access: sess.access});
+}
+else {
+  res.render('index.ejs', {msg: result});
+}
+});
+});
+
+app.get('/admin', function(req, res) {
+  var msg ='';
+res.render('admin.ejs', {msg: msg});
 });
 
 app.get('/adminapplication', function(req, res) {
@@ -31,8 +75,7 @@ res.render('adminappli.ejs', {msg: user});
 
 app.post('/deletuser', function(req, res) {
   var useradelet = req.body.useradelet;
-  reqmysql.deletuser(useradelet, function callback (result){
-  });
+  reqmysql.deletuser(useradelet);
 res.redirect('/adminapplication');
 });
 
@@ -43,28 +86,6 @@ app.post('/ajouteruser', function(req, res) {
   reqmysql.ajoutuser(loginajouter, passwordajouter, access, function callback (result){
   });
 res.redirect('/adminapplication');
-});
-
-app.post('/admin', function(req, res) {
-	var login = req.body.login;
-	var password = req.body.password;
-	reqmysql.selectuser(login, password, function callback (result){
-		try{
-	if (result.login == login && result.password == password) {
-		sess = req.session;
-		sess.access = result.access;
-    res.render('admin.ejs', {access: sess.access});
-  	};
-  	if (result.login == login && result.password != password) {
-  	var msg = 'Mot de passe incorrect';
-    res.render('index.ejs', {msg: msg});
-    };
-  	}
-  	catch(msg){
-  	var msg = 'Le login existe pas.';
-    	res.render('index.ejs', {msg: msg});
-  	}
-	});
 })
 .use(express.static(__dirname + '/css'));
 server.listen(8080);
