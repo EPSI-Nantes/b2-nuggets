e29ffb52a2eb30a5769443f91503e8e9ba6572d2
Antoine Garrec <antoine.garrec@epsi.fr>
2015-04-23 05:28:45 +0200

    V2.6

--- ressource/connection.js
+++ ressource/connection.js
@@ -8,6 +8,8 @@ var connection = mysql.createConnection(
     }
 );
 
+
+//Le login
 var login = function (login, password, callback){
 connection.query('SELECT id, access from user WHERE login = ? AND password = ?',[ login , password],  function(err, rows, fields) {
 try{
@@ -24,6 +26,9 @@ catch(err){
 };
 exports.login = login;
 
+
+//Les listes
+//Liste d'utilisateur
 var afficheruser = function (callback){
   var queryString = 'SELECT id, login from user';
   connection.query(queryString, function(err, rows, fields) {
@@ -32,6 +37,7 @@ var afficheruser = function (callback){
 };
 exports.afficheruser = afficheruser;
 
+//Liste de materiel 
 var lshardware = function (callback){
   var queryString = 'SELECT id, name, ip from equipement';
   connection.query(queryString, function(err, rows, fields) {
@@ -40,14 +46,7 @@ var lshardware = function (callback){
 };
 exports.lshardware = lshardware;
 
-var countservice = function (callback){
-  var queryString = 'SELECT COUNT(*) FROM service';
-  connection.query(queryString, function(err, rows, fields) {
-    callback(rows);
-});
-};
-exports.countservice = countservice;
-
+//Liste de service
 var lsservice = function (callback){
   var queryString = 'SELECT id, name, ip, port from service';
   connection.query(queryString, function(err, rows, fields) {
@@ -56,32 +55,34 @@ var lsservice = function (callback){
 };
 exports.lsservice = lsservice;
 
-var lsserviceaping = function (callback){
-  var queryString = 'SELECT ip, port from service';
-  connection.query(queryString, function(err, rows, fields) {
-    callback(rows);
-});
-};
-exports.lsserviceaping = lsserviceaping;
 
+
+//Les fonction de delet
+//Suprimer un utilisateur
 var deletuser = function (useradelet){ 
   connection.query('DELETE FROM user WHERE id = ? ;', [ useradelet ],  function(err, rows, fields) {
 });
 };
 exports.deletuser = deletuser;
 
+//Suprimer un materiel
 var delethardware = function (hardwareadelet){
   connection.query('DELETE FROM equipement WHERE id = ? ;', [ hardwareadelet ],  function(err, rows, fields) {
 });
 };
 exports.delethardware = delethardware;
 
+//Suprimer un service
 var deletservice = function (serviceadelet){
   connection.query('DELETE FROM service WHERE id = ? ;', [ serviceadelet ],  function(err, rows, fields) {
 });
 };
 exports.deletservice = deletservice;
 
+
+
+//Les fonction d'INSERT
+//Ajouter un utilisateur
 var ajoutuser = function (loginajouter, passwordajouter, access, callback){
 connection.query('INSERT INTO user (login, password, access) VALUES (?, ?, ?);',[ loginajouter , passwordajouter, access],  function(err, rows, fields) {
     if (err) {
@@ -96,12 +97,14 @@ connection.query('INSERT INTO user (login, password, access) VALUES (?, ?, ?);',
 };
 exports.ajoutuser = ajoutuser;
 
+//Ajouter un materiel
 var ajouterhardware = function (nameeq, ip){
 connection.query('INSERT INTO equipement (name, ip) VALUES (?, ?);', [ nameeq , ip],  function(err, rows, fields) {
 });
 };
 exports.ajouterhardware = ajouterhardware;
 
+//Ajouter un service
 var ajouterservice = function (namese, ipse, portse){
 connection.query('INSERT INTO service (name, ip, port) VALUES (?, ?, ?);', [ namese , ipse, portse],  function(err, rows, fields) {
 });
